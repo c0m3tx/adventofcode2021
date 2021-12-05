aa919d455011dcb2f2a5a5abf7e82e66336f5781
Fabio 'c0m3tx' Vitale <cometspk@gmail.com>
2021-12-05 14:10:39 +0100

    Optimize main to use generic Iterator on Line

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,39 +4,30 @@ mod parser;
 use line::Line;
 use line::Point;
 use std::collections::HashMap;
-use std::slice::Iter;
 
-fn filtered_results(lines: Iter<Line>) {
+fn results<'a, I>(lines: I) -> usize
+where
+    I: Iterator<Item = &'a Line>,
+{
     let mut results_map: HashMap<Point, usize> = HashMap::new();
-    lines
-        .filter(|line| line.is_horizontal() || line.is_vertical())
-        .flat_map(|line| line.coverage())
-        .for_each(|p| {
-            let count = results_map.entry(p).or_insert(0);
-            *count += 1;
-        });
-    println!(
-        "Filtered: {}",
-        results_map.iter().filter(|(_, &v)| v > 1).count()
-    );
-}
-
-fn full_results(lines: Iter<Line>) {
-    let mut results_map: HashMap<Point, usize> = HashMap::new();
-    lines.flat_map(|line| line.coverage()).for_each(|p| {
+    lines.flat_map(Line::coverage).for_each(|p| {
         let count = results_map.entry(p).or_insert(0);
         *count += 1;
     });
-    println!(
-        "Full result: {}",
-        results_map.iter().filter(|(_, &v)| v > 1).count()
-    );
+    results_map.iter().filter(|(_, &v)| v > 1).count()
 }
 
 fn main() {
     let input = std::fs::read_to_string("input").expect("Could not read input");
     let lines = parser::lines_parser::file(&input).expect("Error parsing input");
 
-    filtered_results(lines.iter());
-    full_results(lines.iter());
+    let filtered_results = results(
+        lines
+            .iter()
+            .filter(|line| line.is_horizontal() || line.is_vertical()),
+    );
+    let full_results = results(lines.iter());
+
+    println!("Filtered results: {}", filtered_results);
+    println!("Full results: {}", full_results);
 }
